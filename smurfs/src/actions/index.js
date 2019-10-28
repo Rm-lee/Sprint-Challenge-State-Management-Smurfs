56ae43164a5d3c0d960379f4c097b8fe39b25d69
Roger Lee <rmlee0003@gmail.com>
2019-10-27 20:52:53 -0400

    code formatting

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,31 +1,32 @@
-import axios from 'axios'
-export const RETRIEVESMURF = "RETREIVESMURF"
-export const LOADINGSMURFS = "LOADINGSMURFS"
-export const PUSHSMURF = "PUSHSMURF"
+import axios from "axios";
+export const RETRIEVESMURF = "RETREIVESMURF";
+export const LOADINGSMURFS = "LOADINGSMURFS";
+export const PUSHSMURF = "PUSHSMURF";
 
-export function retrieveSmurf(){
- return (dispatch) => {
-  dispatch({type:LOADINGSMURFS})
+export function retrieveSmurf() {
+  return dispatch => {
+    dispatch({ type: LOADINGSMURFS });
 
-  axios.get('http://localhost:3333/smurfs')
-  .then(res => {
-   dispatch({type: RETRIEVESMURF, payload: res.data})
- 
-  })
-  .catch(err => {
-   console.log(err)
-  })
- }
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(res => {
+        dispatch({ type: RETRIEVESMURF, payload: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 }
 
-export function pushSmurf(smurf){
- return (dispatch) => {
-  axios.post('http://localhost:3333/smurfs',smurf)
-  .then(res => {
-   dispatch({type: PUSHSMURF, payload: res.data})
-  })
-  .catch(err => {
-   console.log(err)
-  })
- }
+export function pushSmurf(smurf) {
+  return dispatch => {
+    axios
+      .post("http://localhost:3333/smurfs", smurf)
+      .then(res => {
+        dispatch({ type: PUSHSMURF, payload: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 }
--- smurfs/src/components/AddSmurf.js
+++ smurfs/src/components/AddSmurf.js
@@ -1,57 +1,74 @@
-import React from 'react';
-import {useState} from 'react'
-import { connect } from 'react-redux'
-import {pushSmurf} from '../actions/index'
+import React from "react";
+import { useState } from "react";
+import { connect } from "react-redux";
+import { pushSmurf } from "../actions/index";
 function AddSmurf(props) {
+  const [state, setState] = useState({
+    name: "",
+    age: "",
+    height: ""
+  });
 
- const [state,setState] = useState({
-  name: '',
-  age:'',
-  height:''
- })
+  function submitHandler(e) {
+    e.preventDefault();
+    const { name, age, height } = state;
+    const newSmurf = {
+      name,
+      age,
+      height
+    };
+    console.log(newSmurf);
+    props.pushSmurf(newSmurf);
+  }
+  function handleChange(e) {
+    const value = e.target.value;
+    setState({
+      ...state,
+      [e.target.name]: value
+    });
+  }
 
-
- function submitHandler(e) {
-  e.preventDefault()
-  const {name,age,height} = state
-  const newSmurf = {
-   name,
-   age,
-   height
-  } 
-  console.log(newSmurf)
-  props.pushSmurf(newSmurf)
- }
- function handleChange(e){
-  const value = e.target.value
-  setState({
-   ...state,
-   [e.target.name]:value
-  })
- 
- }
-
- return (
-  <div>
-   <form onSubmit={submitHandler}>
-    <input type="text" name="name" placeholder="name" value={state.name} onChange={handleChange}/><br/>
-    <input type="text" name="age" placeholder="age" value={state.age} onChange={handleChange}/><br/>
-    <input type="text" name="height" placeholder="height" value={state.height} onChange={handleChange}/><br/>
-    <button>Add Smurf</button>
-
-   </form>
-  </div>
- );
- }
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          name="name"
+          placeholder="name"
+          value={state.name}
+          onChange={handleChange}
+        />
+        <br />
+        <input
+          type="text"
+          name="age"
+          placeholder="age"
+          value={state.age}
+          onChange={handleChange}
+        />
+        <br />
+        <input
+          type="text"
+          name="height"
+          placeholder="height"
+          value={state.height}
+          onChange={handleChange}
+        />
+        <br />
+        <button>Add Smurf</button>
+      </form>
+    </div>
+  );
+}
 function mapStateToProps(state) {
- return{
-  smurfs: state.smurfs
- }
+  return {
+    smurfs: state.smurfs
+  };
 }
 const mapDispatchToProps = {
- pushSmurf: pushSmurf
-}
+  pushSmurf: pushSmurf
+};
 export default connect(
- mapStateToProps,
- mapDispatchToProps,
- )(AddSmurf)
+  mapStateToProps,
+  mapDispatchToProps
+)(AddSmurf);
